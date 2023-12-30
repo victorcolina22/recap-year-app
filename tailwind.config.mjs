39e91b2b516d7851305596f067af5e451ebd3d0e
Victor Colina <victorcolina@G500606MAC272.local>
2023-12-30 14:11:47 -0300

    * January page added
    * February page added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import plugin from 'tailwindcss';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -5,14 +7,54 @@ export default {
     extend: {
       animation: {
         'infinite-scroll': 'infinite-scroll 15s linear infinite',
+        'fade': 'fade 1s ease',
+        'fadeUp': 'fadeUp 1s ease',
+        'fadeDown': 'fadeDown 1s ease',
+        'fadeLeft': 'fadeLeft 1s ease',
+        'fadeRight': 'fadeRight 1s ease',
       },
       keyframes: {
         'infinite-scroll': {
           from: { transform: 'translateX(-100%)' },
           to: { transform: 'translateX(0)' },
-        }
-      }                    
+        },
+        'fade': {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
+        'fadeUp': {
+          from: { transform: 'translateY(80%)', opacity: '0' },
+          to: { transform: 'translateY(0)', opacity: '1' },
+        },
+        'fadeDown': {
+          from: { transform: 'translateY(-80%)', opacity: '0' },
+          to: { transform: 'translateY(0)', opacity: '1' },
+        },
+        'fadeLeft': {
+          from: { transform: 'translateX(-20%)', opacity: '0' },
+          to: { transform: 'translateY(0)', opacity: '1' },
+        },
+        'fadeRight': {
+          from: { transform: 'translateX(20%)', opacity: '0' },
+          to: { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
     },
+    screens: {
+      'xs': '580px'
+    }
   },
-	plugins: [],
+	plugins: [
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          "animation-delay": (value) => {
+            return {
+              "animation-delay": value,
+            };
+          },
+        },
+      );
+    }),
+  ],
 }
